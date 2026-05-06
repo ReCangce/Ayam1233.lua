0c9c5aeaa6f48ddc2aa9e5679d8ce9d68c40b222
ReCangce <edwardlijaya@gmail.com>
2026-05-06 19:14:35 +0700

    Update Made by recanggce.lua

--- Made by recanggce.lua
+++ Made by recanggce.lua
@@ -161,7 +161,7 @@ createTab("99 Night", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FoxnameHub.lua"))()
 end)
 
-createTab("Bake or Die", function)
+createTab("Bake or Die", function()
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 

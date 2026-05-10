3dc06f3228ca9b1ce1e7ade679932b5819c59d1b
ReCangce <edwardlijaya@gmail.com>
2026-05-10 20:53:13 +0700

    Update script URL in '99 Night' tab
    Replaced a script URL in the '99 Night' tab with a new one.

--- Made by recanggce.lua
+++ Made by recanggce.lua
@@ -158,8 +158,7 @@ createTab("Infinite Yield", function()
 end)
 
 createTab("99 Night", function()
-        
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FoxnameHub.lua"))()
 end)
 
 createTab("Bake or Die", function()
